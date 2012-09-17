a5b7cb28656ed40d750f6984c109f3efbccaa81e
Janos Pasztor <net@janoszen.hu>
2012-09-17 09:19:06 +0200

    Added more space for the recommend button

--- index.php
+++ index.php
@@ -173,7 +173,7 @@ JavaScript enabled: no
 		fjs.parentNode.insertBefore(js, fjs);
 		}(document, 'script', 'facebook-jssdk'));</script>
 		<div class="fb-like" data-href="http://www.whatismybrowserversion.com/" data-send="false"
-			 data-layout="button_count" data-width="140" data-show-faces="false" data-action="recommend"
+			 data-layout="button_count" data-width="150" data-show-faces="false" data-action="recommend"
 			 data-font="verdana"></div>
 		<a href="https://twitter.com/share" class="twitter-share-button" data-lang="en">Tweet</a>
 		<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="https://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
